d5c1169a988c8ea7b228fc55df6685812a776164
Arshid Aslam Baba <arshidbaba93@gmail.com>
2025-11-10 00:17:39 +0530

    Add db connection code

--- app/database.py
+++ app/database.py
@@ -1,19 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# SQLite DB stored in persistent folder inside container
 DATABASE_URL = "sqlite:////app/data/addresses.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False},  # Required for SQLite
-    echo=False,  # Set True for SQL logging during dev
+    connect_args={"check_same_thread": False},
+    echo=False,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     """Dependency to get DB session."""
@@ -22,3 +20,4 @@ def get_db():
         yield db
     finally:
         db.close()
+        
